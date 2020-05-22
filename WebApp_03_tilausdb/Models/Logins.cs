aa2526ca4c1148aa6e0a72d868f84d79985cd9dc
zkadam <adam.zakar@student.careeria.fi>
2020-05-22 13:11:29 +0300

    Revert "database refreshed for views logins needs to be updated from previous version"
    This reverts commit 0644f24fc6bdf5e99eb14d8d50d84ace99b346d9.

--- WebApp_03_tilausdb/Models/Logins.cs
+++ WebApp_03_tilausdb/Models/Logins.cs
@@ -11,11 +11,16 @@ namespace WebApp_03_tilausdb.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Logins
     {
         public int LoginId { get; set; }
+        [Required(ErrorMessage = "Anna käyttäjätunnus!")]
         public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Anna salasana!")]
         public string PassWord { get; set; }
+        public string LoginErrorMessage { get; set; }
     }
 }
